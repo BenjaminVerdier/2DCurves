eecaace4836b24cd0172a1e1610e18830c64cb80
BenjaminVerdier <ben.verdier@hotmail.fr>
2021-02-19 17:40:15 -0500

    Added command line options for interpolation

--- c1c2_interpolation.py
+++ c1c2_interpolation.py
@@ -3,15 +3,14 @@ import numpy as np
 import mapbox_earcut as earcut
 from stl import mesh
 from linetostl import polarToCart
-
-TOP_Z = 1
+import argparse
 
 theta = np.arange(0, (2 * pi), 0.01)
 
 def radius_comp(radius = 1, c1 = 0, c2 = 0):
     return radius * (1 + c1 * cos(4 * theta) + c2 * cos(8 * theta))
 
-def interpolate_stl(r_1, c1_1, c2_1, r_2, c1_2, c2_2, z = TOP_Z, steps = 1000):
+def interpolate_stl(r_1, c1_1, c2_1, r_2, c1_2, c2_2, z, steps, name):
     tuples = np.linspace((r_1, c1_1, c2_1), (r_2, c1_2, c2_2), steps)
 
     pts_3d = [[]]*steps
@@ -54,6 +53,48 @@ def interpolate_stl(r_1, c1_1, c2_1, r_2, c1_2, c2_2, z = TOP_Z, steps = 1000):
 
     new_mesh = mesh.Mesh(data)
 
-    new_mesh.save('test.stl')
+    new_mesh.save(name)
 
-interpolate_stl(5,-0.1,0.2,4,0.2,-0.3,10,100)
+if __name__ == '__main__':
+    #default values
+    R_1 = 1
+    c1_1 = 0
+    c2_1 = 0
+    R_2 = 1
+    c1_2 = 0
+    c2_2 = 0
+    N = 100
+    H = 1
+    name = ''
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--R_1', type=float, help='Radius of the base, default=1')
+    parser.add_argument('--c1_1', type=float, help='Value of c1 of the base, default=0')
+    parser.add_argument('--c2_1', type=float, help='Value of c2 of the base, default=0')
+    parser.add_argument('--R_2', type=float, help='Radius of the base, default=1')
+    parser.add_argument('--c1_2', type=float, help='Value of c1 of the base, default=0')
+    parser.add_argument('--c2_2', type=float, help='Value of c2 of the base, default=0')
+    parser.add_argument('--N', type=float, help='Number of interpolation steps, default=100')
+    parser.add_argument('--H', type=float, help='Height of the stl, default=1')
+    parser.add_argument('--name', help='Name of the stl file, default="r_xxx_XXX_c1_yyy_YYY_c2_zzz_ZZZ.stl"')
+    args=parser.parse_args()
+    if args.R_1:
+        R_1 = args.R_1
+    if args.c1_1:
+        c1_1 = args.c1_1
+    if args.c2_1:
+        c2_1 = args.c2_1
+    if args.R_2:
+        R_2 = args.R_2
+    if args.c1_2:
+        c1_2 = args.c1_2
+    if args.c2_2:
+        c2_2 = args.c2_2
+    if args.N:
+        N = args.N
+    if args.H:
+        H = args.H
+    if args.name:
+        name = args.name
+    else:
+        name =  'r_{:.2f}'.format(R_1) + '_{:.2f}'.format(R_2) + '_c1_{:.2f}'.format(c1_1) + '_{:.2f}'.format(c1_2) + '_c2_{:.2f}'.format(c2_1) + '_{:.2f}'.format(c2_2) + '.stl'
+    interpolate_stl(R_1, c1_1, c2_1, R_2, c1_2, c2_2, H, N, name)
